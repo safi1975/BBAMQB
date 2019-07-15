314be10f3a5f9d917810bdbca85c9fb720f4d076
Adam Madela <dungald@gmail.com>
2019-07-15 23:52:30 +0200

    report on appliation

--- src/main/java/com/app/dataentry/controllers/MainController.java
+++ src/main/java/com/app/dataentry/controllers/MainController.java
@@ -1,10 +1,20 @@
 package com.app.dataentry.controllers;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.app.dataentry.constants.Role;
 import com.app.dataentry.services.ReportServiceImpl;
 
 @Controller
@@ -15,13 +25,19 @@ public class MainController {
 	
     @GetMapping("/")
     public String main(Model model) {
-    	re.generateReport();
-        return "index";
+       return "index";
     }
 
     @GetMapping("/login")
     public String login(Model model) {
         return "login_page";
     }
-
+    
+    @Secured(value = { Role.ADMIN, Role.OPERATOR })
+    @ResponseBody
+    @GetMapping(value = "/report", produces = MediaType.APPLICATION_PDF_VALUE)
+    public byte[] report(Model model, HttpServletResponse response) {
+    	ByteArrayOutputStream output = re.generateReport();
+    	return output.toByteArray();
+    }
 }
--- src/main/java/com/app/dataentry/services/ReportServiceImpl.java
+++ src/main/java/com/app/dataentry/services/ReportServiceImpl.java
@@ -1,5 +1,10 @@
 package com.app.dataentry.services;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,37 +13,32 @@ import org.springframework.stereotype.Service;
 import com.app.dataentry.report.PageList;
 
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.export.JRPdfExporter;
-import net.sf.jasperreports.export.OutputStreamExporterOutput;
 
 @Service
 public class ReportServiceImpl {
 	
-	public void generateReport() {
+	public ByteArrayOutputStream generateReport() {
 		
 		PageList pageList = new PageList();
 		
 		
 		JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(pageList.getPageList());
-		
-		
-		
+
         Map parameters = new HashMap();
         try {
-               JasperPrint fillReport = JasperFillManager.fillReport("C:\\work\\data-entry-app\\src\\main\\resources\\reports\\report_template.jasper", parameters, beanColDataSource);
-                JRPdfExporter exporter = new JRPdfExporter();
-                exporter.setParameter(JRExporterParameter.JASPER_PRINT, fillReport);
-                exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, "C:\\work\\data-entry-app\\src\\main\\resources\\reports\\report.pdf");
-                exporter.exportReport();
-                OutputStreamExporterOutput exporterOutput = exporter.getExporterOutput();
+        		
+               JasperPrint fillReport = JasperFillManager.fillReport(this.getClass().getClassLoader().getResource("reports/report_template.jasper").getPath(), parameters, beanColDataSource);
+               ByteArrayOutputStream output = new ByteArrayOutputStream(); 
+               JasperExportManager.exportReportToPdfStream(fillReport, output);
+                return output;
         } catch (JRException e) {
                e.printStackTrace();
         }
+        return null;
 	}
 
 }
