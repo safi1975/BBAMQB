d9a2f3b3bbf9a7fb09d582c320e6632e18b8f769
Adam Madela <dungald@gmail.com>
2019-07-17 01:22:21 +0200

    dont save empty rows on multi

--- src/main/java/com/app/dataentry/controllers/ClientController.java
+++ src/main/java/com/app/dataentry/controllers/ClientController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,6 +50,9 @@ public class ClientController {
 	@PostMapping("/client_multi")
 	public String saveMulti(Model model, @ModelAttribute(name = "clients") ClientsDto clientsDto) {
 		for (ClientDto clientDto: clientsDto.getClients()) {
+			if (StringUtils.isEmpty(clientDto.getName())) {
+				continue;
+			}
 			clientService.saveClient(clientDto);
 		}
 		return "redirect:/client";
