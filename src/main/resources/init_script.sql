c0d4410e9ba5a10174aebf4d0f080d65632116f7
Adam Madela <dungald@gmail.com>
2019-07-21 19:32:58 +0200

    delete user

--- src/main/resources/init_script.sql
+++ src/main/resources/init_script.sql
@@ -1,34 +1,37 @@
-DROP TABLE IF EXISTS `app_user`;
+set transaction read write;
+DROP TABLE IF EXISTS app_user;
 
 CREATE SEQUENCE IF NOT EXISTS app_user_seq START WITH 20;
 
-create table `app_user`
+create table app_user
 (
-    `id` int NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    `role` varchar(255) NOT NULL,
-    `mobile_no` varchar(255),
-    `code` varchar(8) DEFAULT NULL,
-    `enabled` int NOT NULL DEFAULT 1,
-    PRIMARY KEY (`id`)
+    id int NOT NULL,
+    name varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    role varchar(255) NOT NULL,
+    mobile_no varchar(255),
+    code varchar(8) DEFAULT NULL,
+    enabled int NOT NULL DEFAULT 1,
+    PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS `client`;
+DROP TABLE IF EXISTS client;
 
 CREATE SEQUENCE IF NOT EXISTS client_seq START WITH 20;
 
-create table `client`
+create table client
 (
-    `id` int NOT NULL,
-    `name` varchar(255) DEFAULT NULL,
-    `mobile_no` varchar(255) DEFAULT NULL,
-    `product` varchar(255) DEFAULT NULL,
-    `created_at` TIMESTAMP(6) DEFAULT NULL,
-    `created_by` varchar(255) DEFAULT NULL,
-    `updated_at` TIMESTAMP(6) DEFAULT NULL,
-    `updated_by` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    id int NOT NULL,
+    name varchar(255) DEFAULT NULL,
+    mobile_no varchar(255) DEFAULT NULL,
+    product varchar(255) DEFAULT NULL,
+    created_at TIMESTAMP(6) DEFAULT NULL,
+    created_by varchar(255) DEFAULT NULL,
+    updated_at TIMESTAMP(6) DEFAULT NULL,
+    updated_by varchar(255) DEFAULT NULL,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO app_user (id, name, password, role, enabled, mobile_no) VALUES (1, 'pyra', '$2a$10$dn6lNXblW6sTchxRZMxzaOviTb9EikgBWk7vD4Sa5ByzMGeM.U9uq', 'ROLE_ADMIN', true, '9870057600');
+INSERT INTO app_user (id, name, password, role, enabled, mobile_no) VALUES (1, 'pyra', '$2a$10$dn6lNXblW6sTchxRZMxzaOviTb9EikgBWk7vD4Sa5ByzMGeM.U9uq', 'ROLE_ADMIN', 1, '9870057600');
+
+commit;
