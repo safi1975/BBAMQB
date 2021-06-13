3c9be16f2227ed6cba820106797ad174aaf39b05
Mohsin-Islam <mohsin@laam.pk>
2021-06-13 14:42:16 +0500

    Update URL

--- src/main/java/com/app/dataentry/services/SmsServiceImpl.java
+++ src/main/java/com/app/dataentry/services/SmsServiceImpl.java
@@ -13,7 +13,7 @@ public class SmsServiceImpl implements SmsService {
 	
 	@Override
 	public String sendSms(String phone, String code) {
-		String url = "http://vas.mobilogi.com/api.php?username=bbamqb&password=D4rkh0lme&route=1&sender=bbamqb&mobile[]="+phone+"&message[]=Your OTP to login is "+code+" - BBAMQB&templateid=1207162306397172384";
+		String url = "http://vas.mobilogi.com/api.php?username=bbamqb&password=D4rkh0lme&route=1&sender=BBAMQB&mobile[]="+phone+"&message[]=Your OTP to login is "+code+"-BBAMQB&templateid=1207162306397172384";
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 		System.out.println(response.getBody());
