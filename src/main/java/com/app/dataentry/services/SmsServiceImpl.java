f15e655e6a25ff2980da41e1ab4d55dd3ee3029c
Mohsin-Islam <mohsin@laam.pk>
2021-06-13 14:39:46 +0500

    Log the API response

--- src/main/java/com/app/dataentry/services/SmsServiceImpl.java
+++ src/main/java/com/app/dataentry/services/SmsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.app.dataentry.services;
 
 import java.util.Random;
+import java.io.*;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,10 @@ public class SmsServiceImpl implements SmsService {
 	
 	@Override
 	public String sendSms(String phone, String code) {
-		String url = "http://vas.mobilogi.com/api.php?username=bbamqb&password=D4rkh0lme&route=1&sender=bbamqb&mobile[]="+phone+"&message[]=Your OTP to login is "+code;
+		String url = "http://vas.mobilogi.com/api.php?username=bbamqb&password=D4rkh0lme&route=1&sender=bbamqb&mobile[]="+phone+"&message[]=Your OTP to login is "+code+" - BBAMQB&templateid=1207162306397172384";
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+		System.out.println(response.getBody())
 		if (response.getBody().contains("msgid")) {
 			return "Code sent to: " + phone;
 		} else {
