fad304dcae17ee8b8b7900a6ed3cb3d6b1b0f72f
safi1975 <safi@serverguru.in>
2024-05-20 10:29:25 -0700

    errors, connection configuration

--- src/main/java/com/app/dataentry/services/SmsServiceImpl.java
+++ src/main/java/com/app/dataentry/services/SmsServiceImpl.java
@@ -12,8 +12,8 @@ public class SmsServiceImpl implements SmsService {
 
 	@Override
 	public String sendSms(String phone, String code) {
-		String url = "http://vas.mobilogi.com/api.php?username=bbamqb&password=D4rkh0lme&route=1&sender=NUVANT&mobile=
-			"+ phone + "&msg=Your OTP to login to Data Entry Portal is + code +. NUVANTECH&templateid=1707171568555983022";
+		String url = "http://vas.mobilogi.com/api.php?username=bbamqb&password=D4rkh0lme&route=1&sender=NUVANT&mobile=" +
+			 phone + "&msg=Your OTP to login to Data Entry Portal is + code +. NUVANTECH&templateid=1707171568555983022";
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 		return "Code sent to: " + phone;
