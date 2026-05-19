647469dafc703d1a378bc42509a1a38a0003c323
safi1975 <107993834+safi1975@users.noreply.github.com>
2026-05-19 19:56:58 +0530

    Update SmsServiceImpl.java

--- src/main/java/com/app/dataentry/services/SmsServiceImpl.java
+++ src/main/java/com/app/dataentry/services/SmsServiceImpl.java
@@ -12,7 +12,7 @@ public class SmsServiceImpl implements SmsService {
 
 	@Override
 	public String sendSms(String phone, String code) {
-		String url = "http://vas.mobilogi.com/api.php?username=bbamqb&password=D4rkh0lme&route=1&sender=NUVANT&mobile[]="+ phone + "&message[]= OTP to login to Data Entry Portal is + code +. NUVANTECH&templateid=1707171644837732653";
+		String url = "http://vas.mobilogi.com/api.php?username=bbamqb&password=D4rkh0lme&route=1&sender=NUVANT&mobile[]="+ phone + "&message[]= Your OTP to login + code +. NUVANTECH&templateid=1207165597510893709";
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 		return "Code sent to: " + phone;
@@ -22,7 +22,7 @@ public class SmsServiceImpl implements SmsService {
 	@Async
 	public void sendOperatorLoggedInSMS(String phone, String operatorName) {
 
-		String url = "http://vas.mobilogi.com/api.php?username=bbamqb&password=D4rkh0lme&route=1&sender=NUVANT&mobile[]="+phone+"&message[]=User + operatorName + has logged into the Data Entry portal. NUVANTECH&templateid=1707171568566975291";
+		String url = "http://vas.mobilogi.com/api.php?username=bbamqb&password=D4rkh0lme&route=1&sender=NUVANT&mobile[]="+phone+"&message[]=User + operatorName + has logged into the Data Entry portal. NUVANTECH&templateid=1207165597506529864";
 
 		RestTemplate restTemplate = new RestTemplate();
 		restTemplate.getForEntity(url, String.class);
